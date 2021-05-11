055a5eccb62205df3e4ea8a963143c02affc8a60
suganraj77 <84036117+suganraj77@users.noreply.github.com>
2021-05-11 22:30:59 +0530

    Add files via upload

---
+++ Task 1/sugan.js
@@ -0,0 +1,44 @@
+console.log(" hello sugan ")
+//printing age comparision
+var age=5,age2='5';
+if(age===age2)
+{
+	console.log("equal")
+}
+else
+{
+	console.log("not equal")  
+}
+  
+// printing numbers from 1 to 10
+for(var i=0;i<10;i++)  
+{
+    console.log(i+1)
+}
+
+{
+	for(var i=0;i<10;i++)
+	{
+       if(i/2==0)
+       {
+		console.log(" prime number")
+	   }
+	
+	else
+	   {
+		 console.log("prime number")
+	   }
+   }
+}
+ 
+ for(i=0;i<100;i++)
+   {
+   	if(i>18)
+   	{
+   		console.log("Eligible to marraige")
+   	}
+   	else
+   	{
+   		console.log("Not eligible to marraige")
+   	}
+   }
